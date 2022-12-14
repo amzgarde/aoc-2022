bf5e0e442d5e761589d810aa23076d7894ce3801
Amy Garde <amsegarde@gmail.com>
2022-12-14 23:25:41 +0000

    Complete day 14

--- day-14/solution.py
+++ day-14/solution.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 
+import os
+
+
 def get_input(version):
     filename = "input.txt"
     if version == "test":
@@ -10,70 +13,67 @@ def get_input(version):
         return reader.read().splitlines()
 
 
-def print_cavern(cavern, version):
-    with open(f"result-{version}.txt", "w") as append:
-        print("=============", file=append)
-        message = f"{'I ': <2}"
+def print_cavern(cavern, version, file = True):
+    folder = "results"
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+        
+    with open(f"{folder}/result-{version}.txt", "w") as append:
+        message = f"{'I ': <3}"
         for col in range(len(cavern[0])):
             message += str(col % 10)
-        print(message, file=append)
+        if file:
+            print(message, file=append)
+        else:
+            print(message)
         for index, each in enumerate(cavern):
-            print(f"{index: <2} {''.join(each)}", file=append)
+            if file:
+                print(f"{index: <2} {''.join(each)}", file=append)
+            else:
+                print(f"{index: <2} {''.join(each)}")
 
 
 def create_formation(input):
-    left = 1000
-    right = 0
-    down = 0
+    x = []
+    y = []
     formation = []
     for rocks in input:
-        tracing = rocks.split(" -> ")
-
         tracing = [
-            [int(each.split(",")[0]), int(each.split(",")[1])] for each in tracing
+            [int(each.split(",")[0]), int(each.split(",")[1])] for each in rocks.split(" -> ")
         ]
         formation.append(tracing)
+        
         for coord in tracing:
-            if coord[0] < left:
-                left = coord[0]
-            if coord[0] > right:
-                right = coord[0]
-            if coord[1] > down:
-                down = coord[1]
-    size = right - left + 1
+            x.append(coord[0])
+            y.append(coord[1])
 
-    print(left)
+    offset = min(x)
+    size = max(x) - offset + 1
 
-    cavern = []
-    d = 0
-    for depth in range(down + 1):
-        cavern.append(["."] * size)
-        d += 1
-
+    cavern = [['.'] * size for _ in range(max(y) + 1)]
+    
     for trace in formation:
         for index in range(1, len(trace)):
-            rock1 = trace[index - 1]
-            rock2 = trace[index]
-            if rock1[0] == rock2[0]:
-                x = rock1[0] - left
-                y = rock1[1]
-                while y <= rock2[1]:
-                    cavern[y][x] = "#"
-                    y += 1
-            if rock1[1] == rock2[1]:
-                x = rock1[0] - left
-                y = rock1[1]
-                dest_x = rock2[0] - left
-
-                if x - dest_x > 0:
-                    while x >= dest_x:
-                        cavern[y][x] = "#"
-                        x -= 1
-                elif x - dest_x < 0:
-                    while x <= dest_x:
-                        cavern[y][x] = "#"
-                        x += 1
-    return cavern, left
+            source = trace[index - 1]
+            dest = trace[index]
+            if source[0] == dest[0]:
+                row = source[0] - offset
+                
+                start = min([source[1], dest[1]])
+                end = max([source[1], dest[1]])
+                
+                for point in range(start, end):
+                    cavern[point][row] = "#"
+            if source[1] == dest[1]:
+                col = source[1]
+                
+                start = min([source[0], dest[0]])
+                end = max([source[0], dest[0]])
+                
+                for point in range(start, end + 1):
+                    point -= offset
+                    cavern[col][point] = "#"
+    return cavern, offset
 
 
 def falling(formation, starting):
@@ -82,11 +82,47 @@ def falling(formation, starting):
     rest = False
 
     while not rest:
-
-        if col == -1 or col == len(formation[0]) - 1 or row == len(formation) - 1:
-            print(formation[row][col - 1])
-            print(row, col)
+        if row == len(formation) - 1:
             return formation, True
+        elif formation[row + 1][col] == ".":
+            row += 1
+        elif col == 0:
+            return formation, True
+        elif formation[row + 1][col - 1] == ".":
+            row += 1
+            col -= 1
+        elif col == len(formation[0]) - 1:
+            return formation, True
+        elif formation[row + 1][col + 1] == ".":
+            row += 1
+            col += 1
+        else:
+            formation[row][col] = "o"
+            rest = True
+    return formation, False
+
+def update_formation(formation, location):
+    new_formation = []
+    for index, each_row in enumerate(formation):
+        if index == len(formation) - 1:
+            each_row.insert(location, "#")
+        else:
+            each_row.insert(location, ".")
+        new_formation.append(each_row)
+    return new_formation
+
+def falling_pt2(formation, starting):
+    row = starting[0]
+    col = starting[1]
+    rest = False
+
+    while not rest:
+        if col == 0:
+            col += 1
+            formation = update_formation(formation, 0)
+            starting[1] += 1
+        elif col == len(formation[0]) - 1:
+            formation = update_formation(formation, len(formation[0]))
 
         if formation[row + 1][col] == ".":
             row += 1
@@ -98,20 +134,20 @@ def falling(formation, starting):
             col += 1
         else:
             formation[row][col] = "o"
+            if [row, col] == starting:
+                return formation, starting, True
             rest = True
-    return formation, False
+    return formation, starting, False
 
 
 def main(version="real"):
     input = get_input(version)
 
-    formation, offset = create_formation(input)
-    print_cavern(formation, version)
-    print("rows = ", len(formation), "cols = ", len(formation[0]))
+    cavern, offset = create_formation(input)
     starting = [0, 500 - offset]
 
     safe = True
-    sand = 0
+    formation = cavern
     while safe:
 
         formation, found = falling(formation, starting)
@@ -119,10 +155,26 @@ def main(version="real"):
 
         if found:
             safe = False
-        else:
-            sand += 1
 
-    print(sand)
+    count_o = ("".join([item for sublist in formation for item in sublist]).count("o"))
+    
+    print("Part 1:", count_o)
+
+    safe = True
+    formation = cavern
+    formation.append(["."]*len(formation[0]))
+    formation.append(["#"]*len(formation[0]))
+    while safe:
+
+        formation, starting, found = falling_pt2(formation, starting)
+        print_cavern(formation, f"{version}-pt2")
+
+        if found:
+            safe = False
+
+    count_o = ("".join([item for sublist in formation for item in sublist]).count("o"))
+    
+    print("Part 2:", count_o)
 
 
 main("test")
